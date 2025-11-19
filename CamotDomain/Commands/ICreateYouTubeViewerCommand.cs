bff3538b570eba35ac051bc0d1580ee69b7616b0
ReyanCamot <143674806+ReyanCamot@users.noreply.github.com>
2025-11-19 08:07:07 +0800

    Add files via upload

---
+++ CamotDomain/Commands/ICreateYouTubeViewerCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CamotIPT.CamotFramework.Models;
+
+namespace CamotIPT.CamotFramework.Commands
+{
+    public interface ICreateYouTubeViewerCommand
+    {
+        Task Execute(YouTubeViewer youTubeViewer);
+    }
+}
