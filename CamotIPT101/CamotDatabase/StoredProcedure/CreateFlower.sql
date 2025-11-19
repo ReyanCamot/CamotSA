b676b1c8f07d8ed9002650bac84477b212b64700
ReyanCamot <143674806+ReyanCamot@users.noreply.github.com>
2025-11-19 08:39:46 +0800

    Add files via upload

---
+++ CamotIPT101/CamotDatabase/StoredProcedure/CreateFlower.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[CreateFlower]
+	@FlowerID INT = NULL,
+    @FlowerName NVARCHAR(50) = NULL, 
+    @Price NVARCHAR(50) = NULL, 
+    @Quantity NVARCHAR(50) = NULL, 
+    @Total NVARCHAR(50) NULL
+AS
+BEGIN
+	INSERT INTO [dbo].[Flower Shop] ([FlowerID], [FlowerName], [Price], [Quantity],[Total])
+	VALUES (@FlowerID,@FlowerName,@Price,@Quantity,@Total);
+END
---
+++ CamotIPT101/CamotDatabase/StoredProcedure/UpdateFlower.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[UpdateMotor]
+	@FlowerID NVARCHAR(50) = NULL, 
+    @FlowerName NVARCHAR(50) = NULL,
+	@Quantity NVARCHAR(50) = NULL,
+	@Price NVARCHAR(50) = NULL,
+	@Total NVARCHAR(50) = NULL
+AS
+BEGIN
+	UPDATE [dbo].[Flower Shop]
+	SET 
+	[FlowerID] = @FlowerID,
+    [FlowerName] = @FlowerName, 
+    [Price] = @Price,
+    [Quantity] = @Quantity,
+	[Total] = @Total
+	WHERE [FlowerID] = @FlowerID;
+END
